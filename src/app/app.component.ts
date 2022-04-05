9c7d92b18215bd6a3160d08d2889f3b8c653fc4d
Jayrajsinh <jayrajsinh@ifourtechnolab.com>
2022-04-05 10:19:36 +0530

    integrated contract

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { ContractService } from './services/contract.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  taskCount: number = 0;
+  todoList: any;
   title = 'FrontEndBlockChain';
-  constructor(private contactService:ContractService){
 
+  constructor(private contactService: ContractService) {}
+  connectToMetaMask() {
+    this.contactService.connectAccount();
   }
-  connectToMetaMask(){
-  this.contactService.connectAccount();
+  async getCurrenTaskList() {
+    await this.contactService.getTaskCount().then((res) => {
+      this.taskCount = Number(res);
+    });
+  }
+  async getTodoList() {
+    await this.contactService.getTodoList(this.taskCount).then((res: any) => {
+      this.todoList = res;
+    });
   }
 }
--- src/app/services/contract.service.ts
+++ src/app/services/contract.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import Web3 from "web3";
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
 import { Subject } from 'rxjs';
+import { todoListAbi } from '../Constant/todoListAbi';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContractService {
   private web3js: any;
   private provider: any;
   private accounts: any;
-  web3Modal
+  web3Modal;
+  contract: any;
+  networkId: any;
 
   private accountStatusSource = new Subject<any>();
   accountStatus$ = this.accountStatusSource.asObservable();
@@ -21,33 +24,68 @@ export class ContractService {
       walletconnect: {
         package: WalletConnectProvider, // required
         options: {
-          infuraId: "INFURA_ID" // required
-        }
-      }
+          infuraId: 'INFURA_ID', // required
+        },
+      },
     };
 
     this.web3Modal = new Web3Modal({
-      network: "mainnet", // optional
+      network: 'local', // optional
       cacheProvider: true, // optional
       providerOptions, // required
       theme: {
-        background: "rgb(39, 49, 56)",
-        main: "rgb(199, 199, 199)",
-        secondary: "rgb(136, 136, 136)",
-        border: "rgba(195, 195, 195, 0.14)",
-        hover: "rgb(16, 26, 32)"
-      }
+        background: 'rgb(39, 49, 56)',
+        main: 'rgb(199, 199, 199)',
+        secondary: 'rgb(136, 136, 136)',
+        border: 'rgba(195, 195, 195, 0.14)',
+        hover: 'rgb(16, 26, 32)',
+      },
     });
   }
 
   async connectAccount() {
-    debugger
     this.web3Modal.clearCachedProvider();
 
-    this.provider = await this.web3Modal.connect(); // set provider
-    this.web3js = new Web3(this.provider); // create web3 instance
-    this.accounts = await this.web3js.eth.getAccounts(); 
-    this.accountStatusSource.next(this.accounts)
+    // this.provider = await this.web3Modal.connect(); // set provider
+    // this.web3js = new Web3(this.provider); // create web3 instance
+    // this.accounts = await this.web3js.eth.getAccounts();
+    // this.accountStatusSource.next(this.accounts)
+    let provider = window.ethereum;
+    if (typeof provider !== 'undefined') {
+      provider
+        .request({ method: 'eth_requestAccounts' })
+        .then((acc: any) => {
+          this.accounts = acc[0];
+          console.log('account', this.accounts);
+        })
+        .catch((err: any) => {
+          console.error(err);
+        });
+      const web3 = new Web3(window.ethereum);
+      this.networkId = await web3.eth.net.getId();
+      this.contract = new web3.eth.Contract(
+        todoListAbi.abi,
+        '0xd9145CCE52D386f254917e481eB44e9943F39138'
+      );
+    }
+    debugger;
+  }
+  async getTaskCount() {
+    const count = await this.contract.methods
+      .todoCount()
+      .call()
+      .then((res: any) => {
+        return res;
+      });
+    return count;
   }
 
+  async getTodoList(index : number) {
+    return await this.contract.methods
+      .getAll()
+      .call()
+      .then((res: any) => {
+        return res;
+      });
+  }
 }
